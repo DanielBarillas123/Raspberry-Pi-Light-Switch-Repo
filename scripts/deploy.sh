cd035997af2b70127a078e8f88a959fb6b0fdca0
Daniel Barillas <barillasd@moravian.edu>
2026-05-20 21:44:28 -0400

    removed redundant part of command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@ git clone https://github.com/DanielBarillas123/Raspberry-Pi-Light-Switch-Repo.gi
 cd Raspberry-Pi-Light-Switch-Repo
 python3 -m venv .venv
 source .venv/bin/activate
-python -m pip install --upgrade pip setuptools wheel
-python -m pip install -r requirements.txt
+pip install --upgrade pip setuptools wheel
+pip install -r requirements.txt
