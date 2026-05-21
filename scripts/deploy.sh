a0ce5a9f1df6c5d506b2c33575dccd01d44153c1
Daniel Barillas <barillasd@moravian.edu>
2026-05-20 21:38:39 -0400

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,6 @@
 git clone https://github.com/DanielBarillas123/Raspberry-Pi-Light-Switch-Repo.git
-source /home/pi/Raspberry-Pi-Light-Switch-Repo/.venv/bin/activate
+cd Raspberry-Pi-Light-Switch-Repo
+python3 -m venv .venv
+source .venv/bin/activate
 python -m pip install --upgrade pip setuptools wheel
 python -m pip install -r requirements.txt
